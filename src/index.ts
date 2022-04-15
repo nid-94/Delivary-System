908150eb120ad70875fbdabbd023aaa6b6712e1f
nid-94 <86736538+nid-94@users.noreply.github.com>
2022-04-15 11:36:58 +0100

    ajout du total temps et distance

--- src/index.ts
+++ src/index.ts
@@ -57,18 +57,42 @@ function initMap(): void {
         ) as HTMLElement;
   
         summaryPanel.innerHTML = "";
-  
+          let total=0;
+          let duration="";
+          let hours=0;
+          let minutes=0;
         // For each route, display summary information.
         for (let i = 0; i < route.legs.length; i++) {
           const routeSegment = i + 1;
-  
+          
           summaryPanel.innerHTML +=
             "<hr><b>Route Segment: " + routeSegment + "</b><br><hr>";
           summaryPanel.innerHTML += route.legs[i].start_address + " to ";
           summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
           summaryPanel.innerHTML +="DISTANCE : "+ route.legs[i].distance!.text + "<br>";
           summaryPanel.innerHTML += "DURATION : "+route.legs[i].duration!.text + "<br><hr>";
+          // hours calcul
+          (route.legs[i].duration!.text.length<=10)?hours +=0:
+          hours +=parseFloat(route.legs[i].duration!.text.substring(0,1));
+          // minutes calcul && test
+          (route.legs[i].duration!.text.length<=10)?minutes +=parseFloat(route.legs[i].duration!.text.substring(0,2)):
+          (route.legs[i].duration!.text.length>=10 &&route.legs[i].duration!.text.length<=14 )?minutes +=parseFloat(route.legs[i].duration!.text.substring(8,10)):
+          minutes +=parseFloat(route.legs[i].duration!.text.substring(9,11));
+          
+          
+          if (minutes>60) {
+            hours+=1;
+            minutes=minutes-60
+          } else {
+            hours+=0;
+          }
+          total += parseFloat(route.legs[i].distance!.text);
         }
+        duration=`${hours} heures ${minutes} minutes`
+        let totale=(document.getElementById( "total") as HTMLElement);
+        totale.innerHTML="total Distance "+total +" km";
+        let duree=(document.getElementById( "duration") as HTMLElement);
+        duree.innerHTML="total Duration "+duration ;
       })
       .catch((e) => window.alert("Directions request failed due to " + status));
   }
